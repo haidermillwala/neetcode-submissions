4ad642134a2c068c8cb1e40a9cd06d3f14d76fc5
haidermillwala <40603200+haidermillwala@users.noreply.github.com>
2026-06-12 13:55:09 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/count-primes/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int countPrimes(int n) {
+        
+
+        boolean[] primes = new boolean[n];
+
+        for(int i=0; i<n; i++) {
+            primes[i] = true;
+        }
+
+        for(int p=2; p*p<n; p++) {
+            if(primes[p]) {
+                for(int j=p*p; j<n; j+=p) {
+                    primes[j] = false;
+                }
+            }
+        }
+
+        int count=0;
+        for(int k=2; k<n; k++) {
+            if(primes[k]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
